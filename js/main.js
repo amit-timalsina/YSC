bec50cf283215c60f8fc599ff6abb1217470707f
Amit Timalsina <amittimalsina14@gmail.com>
2020-10-22 23:12:52 +0545

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-(function($){
-    $('.home-slides, .home-slides-two').owlCarousel({
+(function ($) {
+		// Navbar Menu JS
+		$('.navbar-nav li a').on('click', function(e){
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top - 100
+			}, 1500);
+			e.preventDefault();
+		});
+		$(document).on('click','.navbar-collapse.in',function(e) {
+			if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+				$(this).collapse('hide');
+			}
+		});
+		$('.navbar-nav li a').on('click', function(){
+			$('.navbar-collapse').collapse('hide');
+			$('.burger-menu').removeClass('active');
+		});
+		
+		// Burger Menu
+		$('.burger-menu').on('click', function() {
+			$(this).toggleClass('active');
+		});
+	// Header Sticky
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 100) {
+			$('.navbar').addClass("is-sticky");
+		}
+		else {
+			$('.navbar').removeClass("is-sticky");
+		}
+	});
+	$('.home-slides, .home-slides-two').owlCarousel({
 		loop: true,
 		nav: true,
 		dots: false,
@@ -11,14 +42,14 @@
 			"<i class='flaticon-right'></i>"
 		],
 	});
-	$(".home-slides-two").on("translate.owl.carousel", function(){
+	$(".home-slides-two").on("translate.owl.carousel", function () {
 		$(".main-banner-content .sub-title").removeClass("animated fadeInUp").css("opacity", "0");
 		$(".main-banner-content h1").removeClass("animated fadeInUp").css("opacity", "0");
 		$(".main-banner-content p").removeClass("animated fadeInUp").css("opacity", "0");
 		$(".main-banner-content .btn-box").removeClass("animated fadeInUp").css("opacity", "0");
 		$(".main-banner-image img").removeClass("animated fadeInUp").css("opacity", "0");
 	});
-	$(".home-slides-two").on("translated.owl.carousel", function(){
+	$(".home-slides-two").on("translated.owl.carousel", function () {
 		$(".main-banner-content .sub-title").addClass("animated fadeInUp").css("opacity", "1");
 		$(".main-banner-content h1").addClass("animated fadeInUp").css("opacity", "1");
 		$(".main-banner-content p").addClass("animated fadeInUp").css("opacity", "1");
@@ -26,44 +57,17 @@
 		$(".main-banner-image img").addClass("animated fadeInUp").css("opacity", "1");
 	});
 
-	// Popup Video
-	$('.popup-youtube').magnificPopup({
-		disableOn: 320,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-		fixedContentPos: false
-	});
-
-}(jQuery));
-
-
-	// Go to Top
-	$(function(){
-		// Scroll Event
-		$(window).on('scroll', function(){
-			var scrolled = $(window).scrollTop();
-			if (scrolled > 300) $('.go-top').addClass('active');
-			if (scrolled < 300) $('.go-top').removeClass('active');
-		});  
-		// Click Event
-		$('.go-top').on('click', function() {
-			$("html, body").animate({ scrollTop: "0" },  500);
-		});
-	});
-	
 	// WoW JS
-	$(window).on ('load', function (){
-        if ($(".wow").length) { 
-            var wow = new WOW ({
-                boxClass:     'wow',      // Animated element css class (default is wow)
-                animateClass: 'animated', // Animation css class (default is animated)
-                offset:       20,         // Distance to the element when triggering the animation (default is 0)
-                mobile:       true,       // Trigger animations on mobile devices (default is true)
-                live:         true,       // Act on asynchronously loaded content (default is true)
-            });
-            wow.init();
-        }
-    });
-	
+	$(window).on('load', function () {
+		if ($(".wow").length) {
+			var wow = new WOW({
+				boxClass: 'wow',      // Animated element css class (default is wow)
+				animateClass: 'animated', // Animation css class (default is animated)
+				offset: 20,         // Distance to the element when triggering the animation (default is 0)
+				mobile: true,       // Trigger animations on mobile devices (default is true)
+				live: true,       // Act on asynchronously loaded content (default is true)
+			});
+			wow.init();
+		}
+	});
+}(jQuery));
