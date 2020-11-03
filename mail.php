8fa6be58e0edf4a46a65d29c13f6b300941f2fa9
Apurba Adhikari <erikduke182@gmail.com>
2020-11-03 22:36:02 +0545

    Backend changed from Node to PHP and Contact us page Fixed

---
+++ mail.php
@@ -0,0 +1,38 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$mes = $_POST["message"];
+
+$email_to = "apurba4790@gmail.com";
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();                                            //
+$mail->Host       = 'smtp.gmail.com';                    
+$mail->SMTPAuth   = true;                                   
+$mail->Username   = 'tempmail.ysc@gmail.com';                     
+$mail->Password   = 'yscrecast';                               
+$mail->Port  = 587;
+
+$mail->setFrom('tempmail.ysc@gmail.com');
+$mail->addAddress($email_to);
+
+$mail->isHTML(true); 
+
+$mail->Subject = "Contact Us | $name";
+$mail->Body = "<h1>$name</h1><h2>$email</h2><h2>$phone</h2><h2>$mes</h2>";
+
+$mail->send();
+echo "<script>
+      window.location.href = 'contact.html';
+    </script>";
+
+?>
